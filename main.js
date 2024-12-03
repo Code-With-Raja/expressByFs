1a643c0768c0fdd10fb6c519ff25cc3e6c993605
Code-With-Raja <prabhatkumardas1992@gmail.com>
2024-12-03 07:02:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+// const express = require('express');
+// const app = express();
+ 
+// app.use(express.json());
+// app.use(express.urlencoded ({extended:true})); 
+ 
+ 
+
+// app.get('/', (req, res) => {
+//     res.send("Raja das2");
+// })
+
+// app.get('/AboutUs', (req, res) => {
+//     res.send("About Us");
+// })
+// app.get('/Home/:slug', (req, res) => {
+
+//     console.log(req);
+
+
+//     res.send(`Hello I am ${req.params.slug}`);
+// })
+// app.post('/', (req, res) => {
+//     res.send('Got a POST request')
+//   })
+//   app.put('/user', (req, res) => {
+//     res.send('Got a PUT request at /user')
+//   })
+//   app.delete('/user', (req, res) => {
+//     res.send('Got a DELETE request at /user')
+//   })
+
+// app.use((err, req, res, next)=>{
+//   console.log(err.stack);
+//   res.status(500).send ('somthing broke');
+//   return next();
+  
+// })
+// app.listen(3000);
+
